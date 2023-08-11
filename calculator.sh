60c611c1b2a6f891baf5ca9f46cf35ca1ed4af96
AnupamaRamesh5 <137329019+AnupamaRamesh5@users.noreply.github.com>
2023-08-12 00:39:31 +0530

    Add files via upload

---
+++ calculator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#
+#
+#################################
+#Title: Addition of two numbers
+#
+#Author: ANUPAMA RAMESH
+#
+#DATE: 11th of AUG 2023
+#
+#Version: V.1
+#
+##################################
+
+
+echo "Enter A"
+read A
+echo "Enter B"
+read B
+C=A+B
+echo "C is $C"
